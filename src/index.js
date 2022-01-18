5733acc348374cefa9a4913fb44754f8371af533
Evan Beck <evandrydenbeck@gmail.com>
2022-01-17 21:07:05 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.querySelector('form')
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    handleToDo(e.target["new-task-description"].value)
+  })
+})
+
+function handleToDo(todo) {
+  let p = document.createElement('p')
+  p.innerText = todo
+  let tasks = document.querySelector('#tasks')
+  tasks.appendChild(p)
+}
